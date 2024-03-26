4047c58d0e8873abb5eeb3b6401901de844fd8b3
gurumahan <gurumahan@digivalsolutions.com>
2024-03-26 11:34:27 +0530

    Initial Commit

---
+++ DraggablePopUp_POC/DraggablePopUp_POCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DraggablePopUp_POCApp.swift
+//  DraggablePopUp_POC
+//
+//  Created by Guru Mahan on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct DraggablePopUp_POCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
